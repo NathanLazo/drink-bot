8b1944f4cef250aea9f369c12db4eb4f585d9064
NathanLazo <nathanlazo@outlook.com>
2023-03-21 20:47:35 -0600

    [UPDATE]

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { app } from "../../utils/firebase";
+import { getDatabase, set, ref } from "firebase/database";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const db = getDatabase(app);
+  const reference = ref(db, "pedidos/" + req.body.id);
+  set(reference, {
+    type: "bacacho",
+    done: false,
+  });
+  res.status(200).json({ name: "John Doe" });
 }
